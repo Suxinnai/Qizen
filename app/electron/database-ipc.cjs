970b6e88a39f2ba946d8cb03fbd93ed7fdfd70ab
Suxinnai <144658016+Suxinnai@users.noreply.github.com>
2026-08-21 17:42:29 +0800

    feat(db): expose structured SQLite readback

--- app/electron/database-ipc.cjs
+++ app/electron/database-ipc.cjs
@@ -5,6 +5,7 @@ const {
   getDatabaseStatus,
   closeQizenDatabase,
 } = require("./database.cjs");
+const { readMigrationBundleFromDatabase } = require("./database-readback.cjs");
 
 let database = null;
 
@@ -19,6 +20,9 @@ function registerDatabaseIpc() {
   ipcMain.handle("qizen:db:status", () => getDatabaseStatus(getDatabase()));
   ipcMain.handle("qizen:db:import-bundle", (_event, bundle) =>
     replaceFromMigrationBundle(getDatabase(), bundle)
+  );
+  ipcMain.handle("qizen:db:export-bundle", () =>
+    readMigrationBundleFromDatabase(getDatabase())
   );
 }
 
